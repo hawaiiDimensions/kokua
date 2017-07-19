17f1441590339319d34ddd64524624329dad32cf
Edward Huang <edwardghg@gmail.com>
2017-07-19 00:33:45 -0700

    Bash script to load data upon installation no longer necessary. Alternative approach is to provide expanded data-loading functionality.

--- testconf.sh
+++
@@ -1,6 +0,0 @@
-# kokua R CRAN data package installation shell script
-
-#! /bin/sh
-## sources single file from gitHub repository hawaiiDimensions/kokua
-## need to look into options to download entire sub-directory from kokua.
-wget -O ~/kokuaData "https://github.com/hawaiiDimensions/kokua/blob/master/data/hawaii_state_geol_ageClean.RData?raw=true"
