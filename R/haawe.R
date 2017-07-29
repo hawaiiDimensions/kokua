88f19b90c2ba963bbd2521f427d9ddfb5830bd52
Edward Huang <edwardghg@gmail.com>
2017-07-28 18:51:44 -0700

    added ajr's suggestions

--- R/haawe.R
+++ R/haawe.R
@@ -5,28 +5,23 @@
 #' @details haawe organizes and centralizes geospacial data for later analysis
 #' 
 #' @param url The URL of the data to be downloaded
-#' @param filename A name the user wishes to assign to the data to be downloaded
+#' @param filename A name the user wishes to assign to the data to be downloaded. Defaults to `NULL`
 #' @param extension The file extension of the data to be downloaded
-## ajr: I like the idea of a filename, but I think it should default to NULL, so that if a user
-## doesn't specify the filename, inside the function it will automatically assign a meaningful name
-## to the download.  This together with a couple of other thoughts will probably neccesitate a 
-## different structure to the function, which I outline in `haawe_pseudoCode.R` there's also
-## a few more notes in this file.
 #' 
 # @example 
 # testURL <- 'http://gis.ess.washington.edu/data/raster/tenmeter/hawaii/niihau.zip' 
 # haawe(testURL, 'niihau', '.zip')
 #'
-#' @return Printed confirmation of successful data download or error message
+#' @return File path string and printed confirmation of successful data download or error message
 #'
 #' @author Edward G. Huang <edwardgh@@berkeley.edu>
 #' @export
 
-haawe <- function(url, filename, extension){
+haawe <- function(url, filename = NULL, extension){
     dest <- file.path(.libPaths(), 'kokua', 'data') #  Finds path to /data folder in kokua installation directory
     datafile <- paste(filename, extension, sep = '') 
     if (datafile %in% list.files(dest)) { #  Checks if file already exists in target directory
-        stop(paste(datafile, 'already exists in target directory', sep = '')) 
+        stop(paste(datafile, 'already exists in target directory', sep = ' ')) 
     } else { #  If file does not exist in target directory, proceed with download
         path <- file.path(dest, datafile)
         download.file(url = url, destfile = path, mode = 'wb') # Downloads to target directory
@@ -37,11 +32,7 @@ haawe <- function(url, filename, extension){
         }
     }
     #  If download was successful, the user is notified
-    print(paste(datafile, 'successfully loaded. Downloaded data can be loaded into R by running: data(', path, ')', sep = ''))
-    ## ajr: minor comment---I would suggest using `cat` instead of `print` here; `cat` is more 
-    ## consistent with how messages are returned by other R functions
-    
-    ## ajr: you could also 'silently' return the path to the data like this:
+    cat(paste(datafile, 'successfully loaded. Downloaded data can be loaded into R by running: data(', path, ')', sep = ''))
     invisible(path)
 }
 
