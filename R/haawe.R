3b23464cd72d54b3d5286356fa68262b99726a3e
Edward Huang <edwardghg@gmail.com>
2017-08-02 15:33:19 -0700

    added invisible helper function to generate load strings, added script to construct a list of load strings, changed extension argument to not include ".", removed cleanup script because it allows the directory to bypass the duplicate file check, and other trivial changes

--- R/haawe.R
+++ R/haawe.R
@@ -10,7 +10,7 @@
 #' 
 # @example 
 # testURL <- 'http://gis.ess.washington.edu/data/raster/tenmeter/hawaii/niihau.zip' 
-# haawe(testURL, 'niihau', '.zip')
+# haawe(testURL, 'niihau', 'zip')
 #'
 #' @return File path string and printed confirmation of successful data download or error message
 #'
@@ -19,49 +19,59 @@
 
 haawe <- function(url, name = NULL, ext) {
     dest <- file.path(.libPaths(), 'kokua', 'data', name) #  Finds path to /data folder in kokua installation directory
-    datafile <- paste(name, ext, sep = '') 
-    if (datafile %in% list.files(dest)) { #  Checks if file already exists in target directory
-        stop(paste(datafile, 'already exists in target directory', sep = ' ')) 
+    data <- paste(name, ext, sep = '.') 
+    if (data %in% list.files(dest)) { #  Checks if file already exists in target directory
+        stop(paste(data, 'already exists in target directory', sep = ' ')) 
     } else { #  If file does not exist in target directory, proceed with download
         if (!file.exists(dest)) { #  If folder does not exist in target directory
-            dir.create(dest) # Creates new folder in the /data directory for file
+            dir.create(dest) #  Creates new folder in the /data directory for file
         }
-        path <- file.path(dest, datafile)
-        download.file(url = url, destfile = path, mode = 'wb') # Downloads to target directory
+        path <- file.path(dest, data)
+        download.file(url = url, destfile = path, mode = 'wb') #  Downloads to target directory
         if (length(unzip(path, list = TRUE)) > 0) { #  Checks if file is compressed
             unzip(path, exdir = dest) #  Unzips file to same directory and keeps original compressed file
-            file.remove(path)
-        }
+            # file.remove(path) #  Optional cleanup.
+        } 
         # if `path` is to a single file, figure out its file extension
-        
-        # if `path` is a directory, look inside at what the file extensions are inside the directory 
-        # (might need to recursively do this...that's something we should talk about on Thursday)
+            # if `path` is a directory, look inside at what the file extensions are inside the directory
+            # (might need to recursively do this...that's something we should talk about on Thursday)
+
         # once you know the file extensions, figure out which spatial data reading 
         # function (`raster` or `readOGR`) you need and combine this info, with the filename 
-        # info into a string that, if read by R, would load the data.  
-        # 
-        # For example, 
-        # if you detected a file named `kauai.bil` you would need a string that says:
-        # 'raster(kauai.bil)'
-        # if you detected a file called `hawaii_state_geol_ageClean.shp` you would need:
-        # readOGR('.', 'hawaii_state_geol_ageClean')
+        # info into a string that, if read by R, would load the data. 
         # ....now that you have that (save the string object to a variable called `readFun`)....
-        # use `writeLines` to put together (and save to /data) a simple R script that loads to datafile(s), something like:
-        # writeLines(con = file.path(.libPaths(), 'kokua', 'data', paste0(filename, '.R'), 
-        #                            text = c(sprinftf('oldwd <- setwd(%s)', 
-        #                                              file.path(.libPaths(), 'kokua', 'data')), 
-        #                                     readFun, # this should be a STRING that you make above
-        #                                              # when you figure out the file extension and
-        #                                              # which function is needed for reading in
-        #                                     'setwd(oldwd) \n'))
+        readFun <- mapply(.readSelect, list.files(dest, recursive = TRUE)) #  Constructs lists of strings of load functions for each file
+        readFun <- readFun[!sapply(readFun, is.null)] #  Drops NULL values
         
+        # use `writeLines` to put together (and save to /data) a simple R script that loads to datafile(s), something like:
+        
+        # mapply(writeLines, readFun, MoreArgs = list(con = file.path(.libPaths(), 'kokua', 'data', paste0(name, '.R'),
+        #                                             text = c(sprintf('oldwd <- setwd(%s)', file.path(.libPaths(), 'kokua', 'data')), readFun, # this should be a STRING that you make above
+        #                                                                                                                                        # when you figure out the file extension and
+        #                                                                                                                                        # which function is needed for reading in
+        #                                                                                                  'setwd(oldwd) \n'))))
     }
     #  If download was successful, the user is notified
-    cat(paste(datafile, ' successfully loaded. Downloaded data can be loaded into R by running: data(', path, ')', sep = ''))
+    cat(paste0(data, ' successfully loaded. Downloaded data can be loaded into R by running: data(', path, ')'))
     invisible(path)
 }
 
-.file_ext <- function(path) {
+.readSelect <- function(file) { #  Returns string of correct load function when given spacial filename 
+    # For example, 
+    # if you detected a file named `kauai.bil` you would need a string that says:
+    # 'raster(kauai.bil)'
+    # if you detected a file called `hawaii_state_geol_ageClean.shp` you would need:
+    # readOGR('.', 'hawaii_state_geol_ageClean')
+    
+    ext <- .fileExt(file) #  Finds extension
+    script <- switch(ext,
+                     'bil' = paste0("raster(", file, ")"),
+                     'shp' = paste0("readOGR('.', ", gsub(paste0('.', ext), '', file), ")"))
+                     # support for addtional extensions to be added
+    return(script)
+}
+
+.fileExt <- function(path) { #  Retrieves the file extension of a string
     pos <- regexpr("\\.([[:alnum:]]+)$", path)
     ifelse(pos > -1L, substring(path, pos + 1L), "")
 }
