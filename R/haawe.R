ee45b05bf92c549806ff7b673cba26b49ab93427
Edward Huang <edwardghg@gmail.com>
2017-08-02 00:18:03 -0700

    modified so that haawe now creates a new folder for each loaded file/compressed file and deletes compressed files after unzipping.

--- R/haawe.R
+++ R/haawe.R
@@ -5,8 +5,8 @@
 #' @details haawe organizes and centralizes geospacial data for later analysis
 #' 
 #' @param url The URL of the data to be downloaded
-#' @param filename A name the user wishes to assign to the data to be downloaded. Defaults to `NULL`
-#' @param extension The file extension of the data to be downloaded
+#' @param name A name the user wishes to assign to the data to be downloaded. Defaults to `NULL`
+#' @param ext The file extension of the data to be downloaded
 #' 
 # @example 
 # testURL <- 'http://gis.ess.washington.edu/data/raster/tenmeter/hawaii/niihau.zip' 
@@ -17,22 +17,49 @@
 #' @author Edward G. Huang <edwardgh@@berkeley.edu>
 #' @export
 
-haawe <- function(url, filename = NULL, extension){
-    dest <- file.path(.libPaths(), 'kokua', 'data') #  Finds path to /data folder in kokua installation directory
-    datafile <- paste(filename, extension, sep = '') 
+haawe <- function(url, name = NULL, ext) {
+    dest <- file.path(.libPaths(), 'kokua', 'data', name) #  Finds path to /data folder in kokua installation directory
+    datafile <- paste(name, ext, sep = '') 
     if (datafile %in% list.files(dest)) { #  Checks if file already exists in target directory
         stop(paste(datafile, 'already exists in target directory', sep = ' ')) 
     } else { #  If file does not exist in target directory, proceed with download
+        if (!file.exists(dest)) { #  If folder does not exist in target directory
+            dir.create(dest) # Creates new folder in the /data directory for file
+        }
         path <- file.path(dest, datafile)
         download.file(url = url, destfile = path, mode = 'wb') # Downloads to target directory
         if (length(unzip(path, list = TRUE)) > 0) { #  Checks if file is compressed
             unzip(path, exdir = dest) #  Unzips file to same directory and keeps original compressed file
-            ## ajr: this looks great for now...if we come across files that aren't compressed we
-            ## can deal with that as it arrises
+            file.remove(path)
         }
+        # if `path` is to a single file, figure out its file extension
+        
+        # if `path` is a directory, look inside at what the file extensions are inside the directory 
+        # (might need to recursively do this...that's something we should talk about on Thursday)
+        # once you know the file extensions, figure out which spatial data reading 
+        # function (`raster` or `readOGR`) you need and combine this info, with the filename 
+        # info into a string that, if read by R, would load the data.  
+        # 
+        # For example, 
+        # if you detected a file named `kauai.bil` you would need a string that says:
+        # 'raster(kauai.bil)'
+        # if you detected a file called `hawaii_state_geol_ageClean.shp` you would need:
+        # readOGR('.', 'hawaii_state_geol_ageClean')
+        # ....now that you have that (save the string object to a variable called `readFun`)....
+        # use `writeLines` to put together (and save to /data) a simple R script that loads to datafile(s), something like:
+        # writeLines(con = file.path(.libPaths(), 'kokua', 'data', paste0(filename, '.R'), 
+        #                            text = c(sprinftf('oldwd <- setwd(%s)', 
+        #                                              file.path(.libPaths(), 'kokua', 'data')), 
+        #                                     readFun, # this should be a STRING that you make above
+        #                                              # when you figure out the file extension and
+        #                                              # which function is needed for reading in
+        #                                     'setwd(oldwd) \n'))
+        
     }
     #  If download was successful, the user is notified
-    cat(paste(datafile, 'successfully loaded. Downloaded data can be loaded into R by running: data(', path, ')', sep = ''))
+    cat(paste(datafile, ' successfully loaded. Downloaded data can be loaded into R by running: data(', path, ')', sep = ''))
     invisible(path)
 }
 
+testURL <- 'http://gis.ess.washington.edu/data/raster/tenmeter/hawaii/niihau.zip' 
+haawe(testURL, 'niihau', '.zip')
