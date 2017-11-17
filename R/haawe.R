6b14da1079cced76715fbe283fefea6b7ed17a32
Edward Huang <edwardghg@gmail.com>
2017-11-17 11:02:24 -0800

    debugged haawe for the most part and wrote a function listloaded to find loaded data. However, the in-function eval call only seems to work for "islands" right now.

--- R/haawe.R
+++ R/haawe.R
@@ -20,15 +20,17 @@ haawe <- function(x, keyname = NULL) { # takes key/url and in case of url also a
         if (length(unloaded) == 0) { #  Stop if there are no unloaded matches
             stop('There are no unloaded data for the key specified') 
         } #  If the data aren't all loaded, load them with .loadData
-        mapply(.loadData, unloaded$url, unloaded$key, .fileExt(unloaded$url), file.path(.libPaths(), 'kokua', 'data')) #  Downloads each previously unloaded file with helper function
-        dataKeys[dataKeys$key %in% unloaded$key, 'loaded'] <- TRUE #  If successful, grep the keys in unloaded and change their $loaded value to TRUE
+        loadString <- c(mapply(.loadData, unloaded$url, unloaded$key, .fileExt(unloaded$url), file.path(.libPaths(), 'kokua', 'data'))) #  Downloads each previously unloaded file with helper function
+        # dataKeys[dataKeys$key %in% unloaded$key, 'loaded'] <- TRUE #  If successful, grep the keys in unloaded and change their $loaded value to TRUE
     } else {
         stopifnot(is.character(keyname)) #  Verifies that keyname is a character string
-        .loadData(x, keyname, .fileExt(x), dest = file.path(.libPaths(), 'kokua', 'data', keyname))
-        newRow <- data.frame(key = keyname, url = x, loaded = TRUE)
-        dataKeys <- rbind(dataKeys, newRow)
+        loadString <- .loadData(x, keyname, .fileExt(x), dest = file.path(.libPaths(), 'kokua', 'data'))
+        # newRow <- data.frame(key = keyname, url = x, loaded = TRUE)
+        # dataKeys <- rbind(dataKeys, newRow)
     }
-    save(dataKeys, file = file.path(.libPaths(), 'kokua', 'data', 'dataKeys.RData')) #  Updates dataKeys file to reflect newly loaded data
+    # save(dataKeys, file = file.path(.libPaths(), 'kokua', 'data', 'dataKeys.RData')) #  Updates dataKeys file to reflect newly loaded data
+    # return(eval(parse(text = loadString)))
+    return(loadString)
 }
 
 ## this version of the function uses keyword strings to download data. it needs a place to store
@@ -46,63 +48,48 @@ haawe <- function(x, keyname = NULL) { # takes key/url and in case of url also a
 
 .loadData <- function(url, name, ext, dest) {
     filename <- paste(name, ext, sep = '.') #  Constructs full filename
-    # if (!file.exists(dest)) { #  If folder does not exist in target directory
-    #     dir.create(dest) #  Creates new folder in the /data directory for file
-    # }
+    if (!file.exists(file.path(dest, name))) { #  If folder does not exist in target directory
+        dir.create(file.path(dest, name)) #  Creates new folder in the /data directory for file
+    }
     path <- file.path(dest, filename) #  Constructs filepath
     download.file(url = as.character(url), destfile = path, mode = 'wb') #  Downloads to target directory
     if (length(unzip(path, list = TRUE)) > 0) { #  Checks if file is compressed
-        unzip(path, exdir = dest) #  Unzips file to same directory and keeps original compressed file
-        file.remove(path) #  Optional cleanup.
+        unzip(path, exdir = file.path(dest, name)) #  Unzips file to same directory and keeps original compressed file
+        # file.remove(path) #  Optional cleanup.
     } 
-    # if `path` is to a single file, figure out its file extension
-    # if `path` is a directory, look inside at what the file extensions are inside the directory
-    # (might need to recursively do this)
+    unlink(file.path(dest, name, "__MACOSX"), recursive = TRUE)
+    files <- list.files(path = file.path(dest, name) , recursive = TRUE)
     
-    # once you know the file extensions, figure out which spatial data reading 
-    # function (`raster` or `readOGR`) you need and combine this info, with the filename 
-    # info into a string that, if read by R, would load the data. 
-    # ....now that you have that (save the string object to a variable called `readFun`)....
-    
-    ## #egh 
-    files <- list.files(dest, recursive = TRUE)
     fileInfo <- mapply(.readSelect, files, mapply(.fileExt, files))
     fileInfo <- fileInfo[!sapply(fileInfo, is.null)]
     stopifnot(length(fileInfo) == 1)
     fileInfo <- unlist(fileInfo)
     
-    readFun <- .scriptSelect(fileInfo[1], fileInfo[2], name = name)
+    if (grepl("/", fileInfo[1], fixed = TRUE)) {
+        subdirectory <- gsub("/.*", "", fileInfo[1])
+        fileInfo[1] <- gsub(".*/", "", fileInfo[1])
+    } else {
+        subdirectory <- ''
+    }
+    
+    readFun <- .scriptSelect(fileInfo[1], fileInfo[2])
     ## 
     # readFun <- suppressWarnings(.readSelect(list.files(dest, recursive = TRUE))) #  Constructs lists of strings of load functions for each file
     # readFun <- readFun[!sapply(readFun, is.null)]
     # broswer()
     # use `writeLines` to put together (and save to /data) a simple R script that loads to datafile(s), something like:
-    loadString <- c(sprintf('oldwd <- setwd("%s")', file.path(dest, name)),
-                    paste0(name, ' <- ', readFun), paste0(name, ' <- ', .scriptSelect(fileInfo[1], fileInfo[2], proj = TRUE, name = name)), 
-                    ###TO FILL ##),# this should be a STRING that you make above
-                    # when you figure out the file extension and
-                    # which function is needed for reading in
+    loadString <- c(sprintf('oldwd <- setwd("%s")', paste0(file.path(dest, name, subdirectory))),
+                    paste0(name, ' <- ', readFun), 
+                    .scriptSelect(fileInfo[1], fileInfo[2], proj = TRUE, name = name), 
                     'setwd(oldwd) \n')
-    print(loadString)
-    eval(parse(text = loadString))
+    # print(loadString)
+    
+    # eval(parse(text = loadString))
     #  If download was successful, the user is notified
-    cat(paste0(filename, ' successfully loaded. Downloaded data can be loaded into R by running: data(', name,')'))
+    cat(paste0(filename, ' successfully loaded. Downloaded data may be viewed by running: plot(', name,')'))
+    return(loadString)
     # invisible(paste0(name, '.R'))
 }
-
-# .readSelect <- function(files, proj = FALSE, name = 0) { #  Returns string of correct loador projection function when given spacial filename 
-#     # For example, 
-#     # if you detected a file named `kauai.bil` you would need a string that says:
-#     # 'raster("kauai.bil")'
-#     # if you detected a file called `hawaii_state_geol_ageClean.shp` you would need:
-#     # readOGR('.', 'hawaii_state_geol_ageClean')
-#     
-#     # looks in directory and prioritizes '.bil'
-#     exts <- mapply(.fileExt, files) #  Finds extensions
-#     scripts <- mapply(.scriptSelect, files, exts, proj, name)
-#     scripts <- scripts[!sapply(scripts, is.null)]
-#     return(scripts)
-# }
 
 .readSelect <- function(file, ext) { #  Finds and returns target filename and file extension
     switch(ext,
@@ -115,15 +102,16 @@ haawe <- function(x, keyname = NULL) { # takes key/url and in case of url also a
     if (proj == FALSE) {
         switch(ext,
                'bil' = paste0("raster('", file, "')"),
-               'shp' = paste0("readOGR('.', '", gsub(paste0('/', name, '.', ext), '', file), "')")
+               'shp' = paste0("readOGR('.', '", gsub(paste0(name, '.', ext), '', file), "')")
         # 'tif' = 
         # 'kml' = 
         # support for addtional extensions to be added
         )
     } else {
         switch(ext,
-               'bil' = paste0('projectRaster(', name, ', crs = CRS("', '+proj=utm +zone=4 +datum=NAD83 +units=m +no_defs +ellps=GRS80 +towgs84=0,0,0', '"))'),
-               'shp' = paste0('spTransform(', name,', CRS("', '+proj=utm +zone=4 +datum=NAD83 +units=m +no_defs +ellps=GRS80 +towgs84=0,0,0', '"))')
+               # 'bil' = paste0('projectRaster(', name, ', crs = CRS("', '+proj=utm +zone=4 +datum=NAD83 +units=m +no_defs +ellps=GRS80 +towgs84=0,0,0', '"))'),
+               'bil' = paste0('crs(', name, ') <- "+proj=utm +zone=4 +datum=NAD83 +units=m +no_defs +ellps=GRS80 +towgs84=0,0,0"'),
+               'shp' = paste0(name, ' <- spTransform(', name,', CRS("', '+proj=utm +zone=4 +datum=NAD83 +units=m +no_defs +ellps=GRS80 +towgs84=0,0,0', '"))')
                # 'tif' = 
                # 'kml' = 
                # support for addtional extensions to be added
@@ -131,7 +119,12 @@ haawe <- function(x, keyname = NULL) { # takes key/url and in case of url also a
     }
 }
 
-.fileExt <- function(path) { #  Retrieves the file extension of a string
+.fileExt <- function(path) { #  Retrieves the file ex   tension of a string
     pos <- regexpr("\\.([[:alnum:]]+)$", path)
     ifelse(pos > -1L, substring(path, pos + 1L), "")
+}
+
+listLoaded <- function() {
+    loaded <- list.dirs(path = file.path(.libPaths(), 'kokua', 'data'), recursive = FALSE, full.names = FALSE)
+    return(loaded[loaded != ""])
 }
