e15ec0458af25813822683e678583ab27c91d4e0
ajrominger <ajrominger@gmail.com>
2018-07-13 15:19:26 -0600

    playing with terrain colors for potential application in elevation colors

--- R/colors.R
+++ R/colors.R
@@ -2,14 +2,16 @@ sfiColors <- c('#D68F85', '#005D77', '#51661A', '#D49A34',
                '#58455F', '#AF2F23', '#008E94', '#A27635', 
                '#636051', '#DECEA1', '#D15A2A', '#CCCEC8', '#322B29')
 
+sfiColors <- terrain.colors(30)
 foo <- convertColor(t(col2rgb(sfiColors)) / 255, from = 'sRGB', to = 'Lab')
-
+foo[, 1] <- seq(40, 96, length.out = nrow(foo))
+sfiColors <- rgb(convertColor(foo, from = 'Lab', to = 'sRGB'))
 
 m <- persp(range(foo[, 2]), range(foo[, 3]), matrix(range(foo[, 1]), nrow = 2, ncol = 2), 
            col = 'transparent', border = 'transparent', xlab = 'a', ylab = 'b', zlab = 'L', 
-           theta = 30, phi = 60)
+           theta = 180, phi = 10)
 points(trans3d(foo[, 2], foo[, 3], foo[, 1], pmat = m), col = sfiColors, pch = 16, cex = 2)
-text(trans3d(foo[, 2], foo[, 3], foo[, 1], pmat = m), labels = 1:length(sfiColors), col = 'white', cex = 0.5)
+# text(trans3d(foo[, 2], foo[, 3], foo[, 1], pmat = m), labels = 1:length(sfiColors), col = 'white', cex = 0.5)
 
 
 # different color sets
